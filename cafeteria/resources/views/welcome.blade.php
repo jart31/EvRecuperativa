385eb05fdc97726c378f87a4d6adc69d42e15cee
jart31 <142357617+jart31@users.noreply.github.com>
2023-10-28 15:21:17 -0300

    Subida de imagenes de cafe
    se subiron a la base de datos las imagnes de cafe y 3 sandwich

--- cafeteria/resources/views/welcome.blade.php
+++ cafeteria/resources/views/welcome.blade.php
@@ -1,4 +1,17 @@
 
 <x-app-layout>
 
+    <div class="bg-gray-100 py-16 px-4 sm:px-6 lg:px-8">
+        <div class="text-center">
+            <h2 class="text-base font-semibold text-indigo-600 tracking-wide uppercase">Bienvenidos a</h2>
+            <p class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">Coffe Shop</p>
+            <p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">Disfruta de nuestra amplia variedad de cafés, pastelería y sándwiches. Cada producto es hecho con expericia y dedicación.</p>
+        </div>
+    </div>
+
+    <div class="flex justify-center mt-10">
+        <img src="img/background-cafeteria.jpg" alt="Imagen de la cafetería" class="h-96 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full">
+    </div>
+
 </x-app-layout>
+
